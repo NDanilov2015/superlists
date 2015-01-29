f07d6c6b49dbcaf3ed943bb51ea09ec46dc32a14
Nikita Danilov <danilovchem@mail.ru>
2015-01-29 14:26:01 +0300

    URLs for forms x2, moved code in views + tests, new URL

--- lists/views.py
+++ lists/views.py
@@ -7,13 +7,14 @@ from lists.models import Item
 # View (i.e. controller) for home page
 # Не понимаю, зачем при запросе домашней страницы POST писать? Или он один раз запросится когда первичо вызываешь, и еще раз после нажатия Enter при отправке формы7 (И тогда в БД будет создан элемент)
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-	
 	return render(request, 'home.html')
 
 # View (i.e. controller!) for individual list
 def view_list(request):
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items':items})
+
+# View (i.e.controller!) for a new list
+def new_list(request):
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
